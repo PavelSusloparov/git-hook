ea4039c5e388651758ce306112c7d2744759125a
Pavel <pavel.susloparov@vts.com>
2021-03-31 20:04:15 -0400

    Fix the script

--- scripts/install-prepare-commit.sh
+++ scripts/install-prepare-commit.sh
@@ -6,6 +6,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 git clone https://github.com/greg0ire/git_template ~/.git_template || echo "already installed"
 git config --global init.templatedir "~/.git_template/template"
 
-mv ~/.git_template/template/hooks/prepare-commit-msg.sh ~/.git_template/template/hooks/prepare-commit-msg_base.sh
-cp "${DIR}"/../scripts/prepare-commit-msg.sh ~/.git_template/template/hooks/
-chmod a+x ~/.git_template/template/hooks/prepare-commit-msg.sh
+mv ~/.git_template/template/hooks/prepare-commit-msg ~/.git_template/template/hooks/prepare-commit-msg-bckp
+cp "${DIR}"/../scripts/prepare-commit-msg ~/.git_template/template/hooks/
+chmod a+x ~/.git_template/template/hooks/prepare-commit-msg
